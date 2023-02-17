80f4804eb9d15b3ad14fd008ac9f0edda344367e
noxcho <bubaev.d@roistat.com>
2023-02-17 19:00:55 +0300

    Добавлен Eslint

--- static/server.js
+++ static/server.js
@@ -3,8 +3,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname +'/../dist'));
+app.use(express.static(`${__dirname}/../dist`));
 
-app.listen(PORT, function () {
+app.listen(PORT, () => {
+  /* eslint-disable */
   console.log(`Example app listening on port ${PORT}!`);
+  /* eslint-enable */
 });
