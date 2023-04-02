35d8ab84f72cd80a47eade840b507ace8a7acf1a
noxcho <bubaev.d@roistat.com>
2023-04-02 20:36:55 +0300

    done

--- static/server.js
+++ static/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const history = require('express-history-api-fallback');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(`${__dirname}/../dist`));
+app.use(history('index.html', { root: './dist' }));
 
 app.listen(PORT, () => {
   /* eslint-disable */
